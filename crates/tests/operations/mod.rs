615c77e88a47b526a4ad301bf7674e8d8b359c95
Brendon Tan <brendontan003@gmail.com>
2025-12-26 18:54:19 +0800

    feat: add eth_subscribe e2e tests (#64)
    * add eth_subscribe e2e tests
    
    * rename test
    
    * remove unused code
    
    * include test address in params
    
    * remove comments

--- crates/tests/operations/mod.rs
+++ crates/tests/operations/mod.rs
@@ -6,11 +6,13 @@ mod debug_rpc;
 mod eth_rpc;
 pub mod manager;
 pub mod utils;
+pub mod websocket;
 
 pub use amount::*;
 pub use debug_rpc::*;
 pub use eth_rpc::{BlockId, *};
 pub use manager::*;
 pub use utils::*;
+pub use websocket::*;
 
 pub use jsonrpsee::http_client::HttpClient;
